162ec35e7cab5602ef1cb5baa7d4cef43ec4a05a
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-06-28 16:29:07 +0200

    Added editor coroutines back as dependency.

---
+++ Dependency Manager/Utility/CoroutineUtilityPackageFix.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+
+namespace plugin.asm.dependency_manager.Utility
+{
+
+    static class CoroutineUtilityPackageFix
+    {
+
+        [InitializeOnLoadMethod]
+        static void OnLoad()
+        {
+          _ = UnityEditor.PackageManager.Client.Remove("utility.lazy.coroutines");
+          _ = UnityEditor.PackageManager.Client.Add("com.unity.editorcoroutines@1.0");
+        }
+
+    }
+
+}
