777e6275445a01eaff6a97af90af6a2699ee369f
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-06-28 19:11:42 +0200

    Create EnsureEditorCoroutinesAdded.cs

---
+++ Dependency Manager/Utility/EnsureEditorCoroutinesAdded.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEditor.PackageManager;
+
+namespace plugin.asm.dependency_manager.Utility
+{
+
+    static class EnsureEditorCoroutinesAdded
+    {
+
+        [InitializeOnLoadMethod]
+        static void OnLoad() =>
+            _ = Client.Add("com.unity.editorcoroutines@1.0");
+
+    }
+
+}
