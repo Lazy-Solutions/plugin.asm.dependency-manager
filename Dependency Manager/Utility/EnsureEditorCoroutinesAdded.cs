02c62f262551e548ca089e43b1164f79d4bff004
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-11-21 21:18:59 +0100

    Fixed import loop.

--- Dependency Manager/Utility/EnsureEditorCoroutinesAdded.cs
+++ Dependency Manager/Utility/EnsureEditorCoroutinesAdded.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using UnityEditor;
 using UnityEditor.PackageManager;
 
 namespace plugin.asm.dependency_manager.Utility
@@ -8,8 +9,14 @@ namespace plugin.asm.dependency_manager.Utility
     {
 
         [InitializeOnLoadMethod]
-        static void OnLoad() =>
-            _ = Client.Add("com.unity.editorcoroutines@1.0");
+        static void OnLoad()
+        {
+
+            //Editor coroutine package is a dependency for CoroutineUtility (a direct dependency of ASM, which is embedded)
+            if (!File.ReadAllText("Packages/manifest.json").Contains("com.unity.editorcoroutines"))
+                _ = Client.Add("com.unity.editorcoroutines@1.0");
+
+        }
 
     }
 
